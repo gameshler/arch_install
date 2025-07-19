dce4af24b69dccb440e0191866c0efe5e40cafe5
gameshler <gameshler@gameshlers-MacBook-Pro.local>
2025-07-19 13:49:38 +0300

    feat: system update

---
+++ core/tabs/system-setup/system-update.sh
@@ -0,0 +1,57 @@
+#!/bin/sh -e
+
+source "$COMMON_SCRIPT"
+
+fastUpdate() {
+    case "$PACKAGER" in
+        pacman)
+            install_packages "$helper" rate-mirrors-bin
+
+            printf "%b\n" "Generating a new list of mirrors using rate-mirrors. This process may take a few seconds..."
+
+            if [ -s "/etc/pacman.d/mirrorlist" ]; then
+                sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+            fi
+
+            dtype_local="$DTYPE"
+            if [ "$dtype_local" = "unknown" ]; then
+                dtype_local="arch"
+            fi
+
+            if ! sudo rate-mirrors --top-mirrors-number-to-retest=5 --disable-comments --save /etc/pacman.d/mirrorlist --allow-root "$dtype_local" > /dev/null || [ ! -s "/etc/pacman.d/mirrorlist" ]; then
+                printf "%b\n" "Rate-mirrors failed, restoring backup."
+                sudo cp /etc/pacman.d/mirrorlist.bak /etc/pacman.d/mirrorlist
+            fi
+            ;;
+        *)
+            printf "%b\n" "Unsupported package manager: ${PACKAGER}"
+            exit 1
+            ;;
+    esac
+}
+
+updateSystem() {
+    printf "%b\n" "Updating system packages."
+    case "$PACKAGER" in
+        pacman)
+            sudo "$PACKAGER" -Sy --noconfirm --needed archlinux-keyring
+            "$helper" -Su --noconfirm
+            ;;
+        *)
+            printf "%b\n" "Unsupported package manager: ${PACKAGER}"
+            exit 1
+            ;;
+    esac
+}
+
+updateFlatpaks() {
+    if command_exists flatpak; then
+        printf "%b\n" "Updating flatpak packages."
+        flatpak update -y
+    fi
+}
+
+checkAurHelper
+fastUpdate
+updateSystem
+updateFlatpaks
