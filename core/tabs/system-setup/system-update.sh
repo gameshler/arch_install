8c23889e24bc8a8aedcb169399132de28d41c814
gameshler <jabrabasel2000@gmail.com>
2025-08-03 13:21:52 +0300

    fix

--- core/tabs/system-setup/system-update.sh
+++ core/tabs/system-setup/system-update.sh
@@ -16,7 +16,7 @@ fastUpdate() {
             dtype_local="${DTYPE:-arch}"
             echo "Using rate-mirrors with distro: $dtype_local"
 
-            if ! sudo rate-mirrors "$dtype_local" --top-mirrors-number-to-retest=5 --disable-comments --save /etc/pacman.d/mirrorlist --allow-root > /dev/null || [ ! -s "/etc/pacman.d/mirrorlist" ]; then
+            if ! sudo rate-mirrors --top-mirrors-number-to-retest=5 --disable-comments --save /etc/pacman.d/mirrorlist --allow-root "$dtype_local" > /dev/null || [ ! -s "/etc/pacman.d/mirrorlist" ]; then
                 printf "%b\n" "Rate-mirrors failed, restoring backup."
                 sudo cp /etc/pacman.d/mirrorlist.bak /etc/pacman.d/mirrorlist
 
