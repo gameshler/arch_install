d650485110a14891df27bcdb5aed38a63513d167
gameshler <jabrabasel2000@gmail.com>
2025-08-03 15:36:42 +0300

    fix

--- core/tabs/apps/browsers/firefox.sh
+++ core/tabs/apps/browsers/firefox.sh
@@ -10,7 +10,7 @@ installFirefox() {
                 sudo "$PACKAGER" -S --needed --noconfirm firefox
                 ;;
             *)
-                printf "%b\n" "Unsupported package manager: "$PACKAGER""
+                printf "%b\n" "Unsupported package manager: ""$PACKAGER"""
                 exit 1
                 ;;
         esac
