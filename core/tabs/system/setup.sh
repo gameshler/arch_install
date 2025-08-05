b8ae76b99addc352d241e9c9c2a8b663dc48aca4
Gameshler <jabrabasel2000@gmail.com>
2025-08-05 18:13:31 +0300

    update(setup)

--- core/tabs/system/setup.sh
+++ core/tabs/system/setup.sh
@@ -71,7 +71,7 @@ systemctl enable --now reflector.timer
 checkAurHelper
 printf "%b\n" "Installing AUR packages with yay"
 install_packages "$helper" \
-  postman-bin brave-bin visual-studio-code-bin
+  insomnia-bin brave-bin visual-studio-code-bin
 
 if echo "${gpu_type}" | grep -E "NVIDIA|GeForce"; then
   # check if flatpak is installed
