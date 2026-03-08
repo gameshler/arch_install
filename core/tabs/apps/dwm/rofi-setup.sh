79d106546e7abdd6fa819affb4b18ccb2a2c4e6d
Gameshler <84982733+gameshler@users.noreply.github.com>
2026-03-08 23:07:20 +0200

    Update rofi-setup.sh

--- core/tabs/apps/dwm/rofi-setup.sh
+++ core/tabs/apps/dwm/rofi-setup.sh
@@ -25,19 +25,21 @@ setupRofiConfig() {
     if [ -d "$HOME/.config/rofi" ] && [ ! -d "$HOME/.config/rofi-bak" ]; then
         cp -r "$HOME/.config/rofi" "$HOME/.config/rofi-bak"
     fi
+    
     mkdir -p "$HOME/.config/rofi"
+    
     curl -sSLo "$HOME/.config/rofi/powermenu.sh" https://github.com/gameshler/dwm/raw/main/config/rofi/powermenu.sh
     chmod +x "$HOME/.config/rofi/powermenu.sh"
+    curl -sSLo "$HOME/.config/rofi/repo-finder.sh" https://github.com/gameshler/dwm/raw/main/config/rofi/repo-finder.sh
+    chmod +x "$HOME/.config/rofi/repo-finder.sh"
+    curl -sSLo "$HOME/.config/rofi/bookmarksmenu.sh" https://github.com/gameshler/dwm/raw/main/config/rofi/bookmarksmenu.sh
+    chmod +x "$HOME/.config/rofi/bookmarksmenu.sh"
+    
     curl -sSLo "$HOME/.config/rofi/config.rasi" https://github.com/gameshler/dwm/raw/main/config/rofi/config.rasi
     mkdir -p "$HOME/.config/rofi/themes"
     curl -sSLo "$HOME/.config/rofi/themes/nord.rasi" https://github.com/gameshler/dwm/raw/main/config/rofi/themes/nord.rasi
-    curl -sSLo "$HOME/.config/rofi/themes/sidetab-nord.rasi" https://github.com/gameshler/dwm/raw/main/config/rofi/themes/sidetab-nord.rasi
     curl -sSLo "$HOME/.config/rofi/themes/powermenu.rasi" https://github.com/gameshler/dwm/raw/main/config/rofi/themes/powermenu.rasi
-    curl -sSLo "$HOME/.config/rofi/repo-finder.sh" https://github.com/gameshler/dwm/raw/main/config/rofi/repo-finder.sh
-    curl -sSLo "$HOME/.config/rofi/bookmarksmenu.sh" https://github.com/gameshler/dwm/raw/main/config/rofi/bookmarksmenu.sh
-
-    chmod +x "$HOME/.config/rofi/repo-finder.sh"
-    chmod +x "$HOME/.config/rofi/bookmarksmenu.sh"
+    
 }
 
 
