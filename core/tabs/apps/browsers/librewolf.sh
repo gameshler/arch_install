25fddf1298742d689ce3d3d23f1453f3aa102c54
Gameshler <84982733+gameshler@users.noreply.github.com>
2026-04-09 15:24:21 +0000

    Update librewolf.sh

--- core/tabs/apps/browsers/librewolf.sh
+++ core/tabs/apps/browsers/librewolf.sh
@@ -4,7 +4,7 @@
 
 installLibreWolf() {
     if ! command_exists io.gitlab.librewolf-community && ! command_exists librewolf; then
-        install_packages librewolf-bin
+        install_packages yay librewolf-bin
     else
         printf "%b\n" "LibreWolf Browser is already installed."
     fi
