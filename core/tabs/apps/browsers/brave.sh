a935f5b3cb86d00bef531879e15317d52426f97f
Gameshler <84982733+gameshler@users.noreply.github.com>
2026-04-11 18:51:28 +0000

    Update brave.sh

--- core/tabs/apps/browsers/brave.sh
+++ core/tabs/apps/browsers/brave.sh
@@ -5,7 +5,7 @@
 installBrave() {
     if ! command_exists com.brave.Browser && ! command_exists brave; then
         printf "%b\n" "Installing Brave..."
-        curl -fsS https://dl.brave.com/install.sh | sh
+         install_packages --aur brave-bin
     else
         printf "%b\n" "Brave Browser is already installed."
     fi
