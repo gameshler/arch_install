03a6d123d038552e093a96f987676c5228be0cce
gameshler <rogue@sentinel.localdomain>
2026-04-13 14:20:26 +0300

    ref: gpu drivers

--- core/tabs/system/gpu-driver.sh
+++ core/tabs/system/gpu-driver.sh
@@ -2,7 +2,7 @@
 
 . "$COMMON_SCRIPT"
 
-installLACT() {
+install_lact() {
     if ! command_exists lact; then
         install_packages lact
         sudo systemctl enable --now lactd
@@ -11,33 +11,50 @@ installLACT() {
     fi
 }
 
-installGpuDriver() {
+detect_gpu() {
+    gpu_lines=$(lspci | grep -Ei 'VGA|3D|Display' || true)
 
-    printf "%b\n" "Installing GPU Drivers"
-    gpu_type=$(lspci | grep "VGA|3D|Display")
-    case "$gpu_type" in
-    *NVIDIA* | *GeForce*)
-        echo "Installing NVIDIA drivers"
-        install_packages nvidia-dkms nvidia-utils nvidia-settings cuda nvidia
-        ;;
-    *Radeon* | *AMD*)
-        echo "Installing AMD drivers"
-        install_packages mesa vulkan-radeon libva-mesa-driver lib32-vulkan-radeon lib32-mesa xf86-video-amdgpu lib32-libva-mesa-driver
-        ;;
-    *Integrated\ Graphics\ Controller*)
-        echo "Installing Intel drivers"
-        install_packages mesa vulkan-intel intel-media-driver
-        ;;
-    *Intel\ Corporation\ UHD*)
-        echo "Installing Intel UHD drivers"
-        install_packages libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
-        ;;
-    *)
-        echo "Unknown GPU type: $gpu_type"
-        ;;
-    esac
+    if echo "$gpu_lines" | grep -qi nvidia; then
+        gpu_vendor="nvidia"
+    elif echo "$gpu_lines" | grep -Eqi 'amd|ati|advanced micro devices'; then
+        gpu_vendor="amd"
+    elif echo "$gpu_lines" | grep -qi intel; then
+        gpu_vendor="intel"
+    elif echo "$gpu_lines" | grep -Eqi 'intel|intel corporation|UHD'; then
+        gpu_vendor="intel-uhd"
+    else
+        echo "Unsupported GPU:"
+        echo "$gpu_lines"
+        exit 1
+    fi
+
+    echo "Detected GPU: $gpu_vendor"
 
 }
 
-installGpuDriver
-installLACT
+install_gpu_drivers() {
+
+    printf "%b\n" "Installing GPU Drivers $gpu_vendor"
+    case "$gpu_vendor" in
+    nvidia)
+        echo "Installing NVIDIA drivers"
+        install_packages nvidia-dkms nvidia-utils nvidia-settings cuda
+        ;;
+    amd)
+        echo "Installing AMD drivers"
+        install_packages mesa vulkan-radeon libva-mesa-driver lib32-vulkan-radeon lib32-mesa xf86-video-amdgpu lib32-libva-mesa-driver
+        ;;
+    intel)
+        echo "Installing Intel drivers"
+        install_packages mesa vulkan-intel intel-media-driver
+        ;;
+    intel-uhd)
+        echo "Installing Intel UHD drivers"
+        install_packages libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
+        ;;
+    esac
+}
+
+detect_gpu
+install_gpu_drivers
+install_lact
