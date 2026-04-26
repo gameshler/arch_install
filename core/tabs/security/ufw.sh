9553dd733630a64c597b2bab1753ba4ac9e6d6f3
Gameshler <84982733+gameshler@users.noreply.github.com>
2026-04-26 16:01:19 +0000

    Update ufw.sh

--- core/tabs/security/ufw.sh
+++ core/tabs/security/ufw.sh
@@ -35,21 +35,24 @@ configure_ufw() {
 configure_sysctl() {
     sudo bash -c '
     cat > /etc/sysctl.d/90-network.conf << EOF
-net.ipv4.ip_forward = 1
-net.ipv6.conf.all.forwarding = 1
+# Do not act as a router
+net.ipv4.ip_forward = 0
+net.ipv6.conf.all.forwarding = 0
 
+# SYN flood protection
 net.ipv4.tcp_syncookies = 1
 
+# Disable ICMP redirect
 net.ipv4.conf.all.accept_redirects = 0
 net.ipv4.conf.default.accept_redirects = 0
+net.ipv4.conf.all.secure_redirects = 0
+net.ipv4.conf.default.secure_redirects = 0
 net.ipv6.conf.all.accept_redirects = 0
 net.ipv6.conf.default.accept_redirects = 0
 
+# Do not send ICMP redirects
 net.ipv4.conf.all.send_redirects = 0
 net.ipv4.conf.default.send_redirects = 0
-
-net.ipv4.conf.all.accept_source_route = 0
-net.ipv6.conf.all.accept_source_route = 0
 
 EOF
     sysctl --system
