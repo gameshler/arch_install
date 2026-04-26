6b0837091a13a256362cb91047aa1c7ff4f21aac
Gameshler <84982733+gameshler@users.noreply.github.com>
2026-04-26 16:00:15 +0000

    Update nftables.sh

--- core/tabs/security/nftables.sh
+++ core/tabs/security/nftables.sh
@@ -101,21 +101,24 @@ EOF
 systemctl enable --now nftables
 
     cat > /etc/sysctl.d/90-network.conf << EOF
-net.ipv4.ip_forward = 1
-net.ipv6.conf.all.forwarding = 1
+# Do not act as a router
+net.ipv4.ip_forward = 0
+net.ipv6.conf.all.forwarding = 0
 
+# SYN flood protection
 net.ipv4.tcp_syncookies = 1
 
+# Disable ICMP redirect
 net.ipv4.conf.all.accept_redirects = 0
 net.ipv4.conf.default.accept_redirects = 0
+net.ipv4.conf.all.secure_redirects = 0
+net.ipv4.conf.default.secure_redirects = 0
 net.ipv6.conf.all.accept_redirects = 0
 net.ipv6.conf.default.accept_redirects = 0
 
+# Do not send ICMP redirects
 net.ipv4.conf.all.send_redirects = 0
 net.ipv4.conf.default.send_redirects = 0
-
-net.ipv4.conf.all.accept_source_route = 0
-net.ipv6.conf.all.accept_source_route = 0
 
 EOF
     sysctl --system
